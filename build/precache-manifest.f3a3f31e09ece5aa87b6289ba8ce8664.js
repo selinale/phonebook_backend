3a4ac5a0590cadaebd00515197cc17e072a95bd4
Selina <selinale@cs.helsinki.fi>
2022-04-03 23:05:11 +0300

    changes

---
+++ build/precache-manifest.f3a3f31e09ece5aa87b6289ba8ce8664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1fde883b24179fc9484",
+    "url": "/static/css/main.e76ea96c.chunk.css"
+  },
+  {
+    "revision": "c1fde883b24179fc9484",
+    "url": "/static/js/main.f428fb25.chunk.js"
+  },
+  {
+    "revision": "6d12d263ae2f31cbd513",
+    "url": "/static/js/runtime~main.8139e1e3.js"
+  },
+  {
+    "revision": "a985d05bfad2491e8522",
+    "url": "/static/js/2.179522b2.chunk.js"
+  },
+  {
+    "revision": "5d06bb858cc186fdb179",
+    "url": "/static/js/3.b019360d.chunk.js"
+  },
+  {
+    "revision": "1f015a1305de28de9e521ea28d308fea",
+    "url": "/index.html"
+  }
+];
