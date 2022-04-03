32cea856d12aef53741f1718e19f57161573b52d
Selina <selinale@cs.helsinki.fi>
2022-04-03 19:11:51 +0300

    add exercises 3.3-3.6

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+const generateId = () => {
+    const id = Math.floor(Math.random() * 50000)
+    return id
+}
+
 let persons = [
     {
         "id": 1,
@@ -32,6 +39,61 @@ app.get('/info', (req, res) => {
 
 app.get('/api/persons', (req, res) => {
     res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    res.status(204).end()
+})
+
+app.post('/api/persons', (req,res) => {
+    const body = req.body
+
+    const personName = persons.map(function(person) {
+        return person.name
+    })
+
+    console.log(persons.includes('Arto Hellas'))
+
+    if (!body.name) {
+        return res.status(400).json({
+            error: 'name missing'
+        })
+    } else if (!body.number) {
+        return res.status(400).json({
+            error: 'number missing'
+        })
+    } else if (personName.includes(body.name)) {
+        return res.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    console.log(persons)
+
+    const person = {
+        id: generateId(),
+        name: body.name,
+        number: body.number
+    }
+    persons = persons.concat(person)
+    
+    res.json(person)
+
+    console.log(persons)
 })
 
 const PORT = 3001
