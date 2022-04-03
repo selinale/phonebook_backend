7bb20f11e9a98e58c035d3b9b5903e551b246a57
Selina <selinale@cs.helsinki.fi>
2022-04-03 22:04:23 +0300

    changes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('request', function (tokens, req, res) {
     return JSON.stringify(tokens, req, res)
@@ -109,7 +111,7 @@ app.post('/api/persons', (req,res) => {
 
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
