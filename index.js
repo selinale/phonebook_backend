0c361753e05cf66eee921f3ebe288128f9dad535
Selina <selinale@cs.helsinki.fi>
2022-04-24 21:27:21 +0300

    add exercise 3.19

--- index.js
+++ index.js
@@ -146,8 +146,9 @@ app.post('/api/persons', (req,res, next) => {
         number: body.number
     })
     
-    person.save().then(savedPerson => {
-        res.json(savedPerson)
+    person.save()
+        .then(savedPerson => {
+            res.json(savedPerson)
     })
     .catch(error => next(error))
 })
