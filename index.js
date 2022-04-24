077cdcdb9c137979d2f0e262856417396a3f3335
Selina <selinale@cs.helsinki.fi>
2022-04-24 21:27:00 +0300

    add exercise 3.19

--- index.js
+++ index.js
@@ -107,28 +107,26 @@ app.delete('/api/persons/:id', (req, res, next) => {
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const body = req.body
+    const {name, number} = req.body
 
-    const person = {
+/*     const person = {
         id: generateId(),
         name: body.name,
         number: body.number
-    }
+    } */
 
-    Person.findByIdAndUpdate(req.params.id, person, { new: true})
+    Person.findByIdAndUpdate(
+        req.params.id, 
+        {name, number}, 
+        { new: true, runValidators: true, context: 'query'})
         .then(updatedPerson => {
             res.json(updatedPerson)
         })
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (req,res) => {
+app.post('/api/persons', (req,res, next) => {
      const body = req.body
-
-/*     const personName = persons.map(function(person) {
-        return person.name
-    }) */
-
  
     if (!body.name) {
         return res.status(400).json({
@@ -142,12 +140,6 @@ app.post('/api/persons', (req,res) => {
         })
     } 
 
-    /*     } else if (personName.includes(body.name)) {
-        return res.status(400).json({
-            error: 'name must be unique'
-        })
-    } */
-
     const person = new Person({
         id: generateId(),
         name: body.name,
@@ -157,7 +149,7 @@ app.post('/api/persons', (req,res) => {
     person.save().then(savedPerson => {
         res.json(savedPerson)
     })
-
+    .catch(error => next(error))
 })
 
 const unknownEndpoint = (req, res) => {
@@ -171,6 +163,8 @@ const errorHandler = (error, req, res, next) => {
 
     if (error.name === 'CastError') {
         return res.status(400).send({ error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).json({ error: error.message})
     }
 
     next(error)
